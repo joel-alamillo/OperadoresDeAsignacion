5a8ec29682c3a043302c3e1357292c90db2246f8
Joel Alamillo <alamillojuarez@gmail.com>
2024-04-02 07:53:17 -0600

    Ejemplo de asignar una variable y operadores

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,32 @@ public class Main {
     public static void main(String[] args) {
     int a =3 , b=2;
     int c = a + 5 - b;
+        System.out.println("c = " + c);
+
+        //incremento de a en uno
+    a += 1; // a = a + 1
+        System.out.println("a = " + a);
+
+        //incremento de a en 3
+    a += 3; // a = a + 3
+        System.out.println("a = " + a);
+
+        //decremento de a en 2
+        a -=2; // a = a -2
+        System.out.println("a = " + a);
+
+        //Operado de multiplicacion
+        a *= 10; // a = a * 10
+        System.out.println("a = " + a);
+
+        //operador de division
+        a /= 2; // a= a / 2
+        System.out.println("a = " + a);
+
+        //operador de modulo
+        a %= 2; // a = a % 2
+        System.out.println("a = " + a);
+
 
     }
 }
